f7f00d4c8e219c9c6017ad499f7bb14200b487ef
sohasikataria3 <sohasikataria123@gmail.com>
2024-09-27 01:45:55 +0500

    Updated Code

---
+++ server apis/routers/rest.route.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const restController = require('../controller/rest.controller');
+
+router.post("/registerrest",restController.registerrest);
+router.post("/getrest",restController.getrest);
+router.post("/updaterest",restController.updaterest);
+router.post("/getallrest",restController.getallrest);
+router.post("/addreview",restController.addreview);
+
+module.exports = router;
